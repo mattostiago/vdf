dfa8f89bf9212a75e940b0711c64948f0553e8ef
Tiago Mattos <tiagomattos2@gmail.com>
2022-11-12 00:13:22 -0300

    Slide em Fotos

--- lib/source/screens/fotos.dart
+++ lib/source/screens/fotos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:vdf/source/components/botoes.dart';
 import 'package:vdf/source/components/modal.dart';
 import 'package:vdf/source/data/dados.dart';
@@ -30,39 +31,55 @@ class _FotosState extends State<Fotos> {
       ),
       floatingActionButton: botao.retornaBotaoFlutuanteWhatsApp(),
       body: Center(
-        child: GridView.builder(
-            itemCount: imagem.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              childAspectRatio: 9 / 12,
-              crossAxisCount: 3,
-              //crossAxisSpacing: 5.0,
-              //mainAxisSpacing: 1.0,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                elevation: 10,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0)),
-                child: InkWell(
-                  child: Container(
-                    height: 120.0,
-                    width: 120.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                        image: AssetImage(imagem[index].imagem),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  onTap: () {
-                    modal.chamaModalImagens(context, imagem[index]);
-                    //Navigator.of(context).push(MaterialPageRoute( builder: (context) => DetalhesFoto(imagem: imagem[index])));
-                  },
+          child: Column(
+        children: [
+          ImageSlideshow(
+              indicatorColor: cor1,
+              onPageChanged: (value) {
+                //debugPrint('Page changed: $value');
+              },
+              height: MediaQuery.of(context).size.height * 0.4,
+              indicatorRadius: 2,
+              autoPlayInterval: 3000,
+              isLoop: true,
+              children: dados.pegarSlideSobre()),
+          const SizedBox(height: 8),
+          Expanded(
+            child: GridView.builder(
+                itemCount: imagem.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 9 / 12,
+                  crossAxisCount: 3,
+                  //crossAxisSpacing: 5.0,
+                  //mainAxisSpacing: 1.0,
                 ),
-              );
-            }),
-      ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: InkWell(
+                      child: Container(
+                        height: 120.0,
+                        width: 120.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                            image: AssetImage(imagem[index].imagem),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        modal.chamaModalImagens(context, imagem[index]);
+                        //Navigator.of(context).push(MaterialPageRoute( builder: (context) => DetalhesFoto(imagem: imagem[index])));
+                      },
+                    ),
+                  );
+                }),
+          ),
+        ],
+      )),
     );
   }
 }
